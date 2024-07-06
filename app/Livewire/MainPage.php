9e184dc2bda9e303979dd02c32ebecd2281226bc
Christopher Falkner <christopher.falkner@web.de>
2024-07-06 12:05:25 +0000

    feat: add search on main page

--- app/Livewire/MainPage.php
+++ app/Livewire/MainPage.php
@@ -2,12 +2,22 @@
 
 namespace App\Livewire;
 
+use App\Models\Category;
+use App\Models\Resolution;
+use Livewire\Attributes\Url;
 use Livewire\Component;
 
 class MainPage extends Component
 {
+
+    #[Url]
+    public ?string $search = '';
+
     public function render()
     {
-        return view('livewire.main-page');
+        return view('livewire.main-page', [
+            'categories' => Category::get(),
+            "resolutions" => Resolution::search($this->search)->get()
+        ]);
     }
 }
--- resources/views/livewire/main-page.blade.php
+++ resources/views/livewire/main-page.blade.php
@@ -1,6 +1,31 @@
 <div>
-    {{-- The Master doesn't talk, he acts. --}}
     <h2>
         Welcome to the main page
     </h2>
+    <div id="search">
+        <input type="text" wire:model.live="search" placeholder="Beschluss suchen">
+    </div>
+    <h3>
+        Suchergebnisse für "{{ $search }}"
+    </h3>
+    <pre>
+        {{ $resolutions }}
+    </pre>
+    <div id="categories">
+        <h3>Categories</h3>
+        @foreach ($categories as $category)
+            @php
+                /** @var App\Models\Category $category */
+            @endphp
+            <div>
+                <h3>
+                    {{ $category->name }}
+                </h3>
+                <span>
+                    {{ $category->id }}
+                </span>
+            </div>
+        @endforeach
+
+    </div>
 </div>
