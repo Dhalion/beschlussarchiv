f4c1fcfc654fe9098399f41778f7df14a42e8397
Christopher Falkner <christopher.falkner@web.de>
2024-07-22 19:41:32 +0000

    fix: relations in seeders

--- database/seeders/ResolutionSeeder.php
+++ database/seeders/ResolutionSeeder.php
@@ -2,10 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Applicant;
 use App\Models\Resolution;
 use Database\Factories\ResolutionFactory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Log;
 
 class ResolutionSeeder extends Seeder
 {
@@ -14,6 +16,19 @@ class ResolutionSeeder extends Seeder
      */
     public function run(): void
     {
-        Resolution::factory()->count(30)->create();
+        $resolutions = Resolution::factory()->count(30)->create();
+
+        $applicants = Applicant::all();
+
+        $resolutions->each(function (Resolution $resolution) use ($applicants) {
+            // Ensure we're getting an array of UUID strings
+            $randomApplicantUuids = $applicants->random(rand(1, 5))->pluck('id')->all();
+
+            // Debugging: Log the UUIDs to ensure they're what we expect
+            Log::debug('Attaching Applicant UUIDs to Resolution:', $randomApplicantUuids);
+
+            // Attach the applicants using their UUIDs
+            $resolution->applicants()->attach($randomApplicantUuids);
+        });
     }
 }
