a98f8fbf1b686f93e8150d09d45df02b2e0185a6
Christopher Falkner <christopher.falkner@web.de>
2024-07-24 14:19:01 +0000

    feat(frontend): setup mary-ui

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="description" content="Beschlusswiki höhö">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     {{ $head ?? '' }}
 
     @stack('head')
