dfe0b4f1e39ea7c5c5f7271c895f09ab54347faf
Christopher Falkner <christopher.falkner@web.de>
2024-07-29 19:18:47 +0000

    feat(frontend): style resolution page

--- resources/views/page/resolution.blade.php
+++ resources/views/page/resolution.blade.php
@@ -1,33 +1,74 @@
-<div>
-    @php
-        /** @var App\Models\Resolution $resolution */
-    @endphp
-    <div class="resolution-head">
-        <div>
-            <h2>
-                {{ $resolution->tag }}
-            </h2>
-            <h3>
-                {{ $resolution->year }}
-            </h3>
+@php
+    /** @var App\Models\Resolution $resolution */
+@endphp
+
+@extends('layouts.app')
+
+@section('title', strtoupper($resolution->tag) . "-$resolution->year Beschlusswiki")
+
+@section('content')
+    <div id="resolution-page" class="flex flex-col w-full md:w-9/12 lg:w-8/12 xl:w-7/12 xl:mx-auto shadow">
+
+        {{-- ###        YEAR-TAG & TITLE HEADER        ### --}}
+        <div id="resolution-head" class="flex justify-center text-black dark:text-slate-200 w-full flex-grow">
+            <div
+                class="bg-hellrosa-200 dark:bg-hellrosa-900 text-beere-600
+             dark:text-beere-200 flex justify-center justify-self-center
+              place-items-center w-full">
+                <div class="flex flex-col items-center xl:p-10 p-5">
+                    <span class="font-black xl:text-8xl text-4xl xl:pb-4">{{ strtoupper($resolution->tag) }}</span>
+                    <span class="font-black xl:text-6xl text-2xl">{{ $resolution->year }}</span>
+                </div>
+                <div class="w-3/4 xl:p-1 font-bold xl:text-4xl text-lg xl:line-clamp-3 xl:text-clip xl:pr-4">
+                    {{ $resolution->title }}
+                </div>
+            </div>
         </div>
-        <h2>
-            {{ $resolution->title }}
-        </h2>
-    </div>
-    <div class="resolution-meta">
-        <span>
-            Kategorie:
-            <a href="/category/{{ $resolution->category_id }}" wire:navigate.hover>
-                {{ $resolution->category->name }}
+
+        {{-- ###        RESOLUTION META        ### --}}
+        <div id="resolution-meta" class="bg-beere-600 dark:bg-beere-200 flex justify-between items-center p-1 px-3 h-6">
+            <div class="text-xs font-sans text-white dark:text-hellrosa-900 antialiased" v-if="category">
+                <a href="/category/{{ $resolution->category->id }}" wire:navigate.hover>
+                    {{ $resolution->category->name }}
+                </a>
+            </div>
+            <div class="text-xs font-sans text-white dark:text-hellrosa-900 antialiased" v-if="applicants">
+                @foreach ($resolution->applicants as $applicant)
+                    <a href="/applicant/{{ $applicant->id }}" wire:navigate.hover>
+                        {{ $applicant->name }}
+                    </a>
+                    @if (!$loop->last)
+                        ,
+                    @endif
+                @endforeach
+            </div>
+        </div>
+
+        {{-- ###        RESOLUTION NAVIGATION        ### --}}
+        <div id="resolution-navigation" class="flex flex-row justify-between m-2 text-beere-400">
+            <a>
+                <x-icon name="o-backward" />
+                Vorheriger Beschluss
             </a>
-        </span>
+            <a wire:navigate href="{{ url()->previous() }}" class="hover:cursor-pointer">
+                <x-icon name="o-arrow-up-on-square" />
+                Zurück zur Übersicht
+            </a>
+            <a wire:navigate href="/category/{{ $resolution->category->id }}">
+                Zurück zur Kategorie
+                <x-icon name="o-queue-list" />
+            </a>
+            <a>
+                Nächster Beschluss
+                <x-icon name="o-forward" />
+            </a>
+        </div>
 
-    </div>
-    <div class="resolution-navigation">
+        {{-- ###        RESOLUTION TEXT        ### --}}
+        <div class="leading-6 antialiased sm:w-3/4 w-5/6 mx-auto pb-10 mt-3">
+            <span class="prose text-base text-gray-800 dark:text-slate-400">
+                {!! $resolution->text !!}
+            </span>
+        </div>
 
-    </div>
-    <div class="resolution-content">
-        {{ $resolution->text }}
-    </div>
-</div>
+    @endsection
