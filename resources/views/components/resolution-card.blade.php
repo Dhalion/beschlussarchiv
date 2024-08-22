fad4b4a5651ff2ca454609eb3444fd9d70ee886f
Christopher Falkner <christopher.falkner@web.de>
2024-08-22 13:55:45 +0000

    feat(search): center resolution tag

--- resources/views/components/resolution-card.blade.php
+++ resources/views/components/resolution-card.blade.php
@@ -5,7 +5,7 @@
     class="w-full flex mx-auto bg-white dark:bg-slate-700 dark:shadow-gray-800 rounded-xl shadow-md hover:shadow-xl overflow-hidden p-3"
     href="/resolution/{{ $resolution->id }}" wire:navigate.hover="resolution">
     <span
-        class="xl:w-1/12 md:w-2/12 w-3/12 mr-3 flex flex-col text-center text-2xl xl:text-3xl text-slate-700 dark:text-slate-200">
+        class="xl:w-1/12 md:w-2/12 w-3/12 mr-3 flex flex-col items-center justify-center text-2xl xl:text-3xl text-slate-700 dark:text-slate-200">
         {{ strtoupper($resolution->tag) }}
     </span>
     </span>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <meta name="description" content="Beschlussarchiv höhö">
+    <meta name="description" content="Beschlussarchiv">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     {{ $head ?? '' }}
