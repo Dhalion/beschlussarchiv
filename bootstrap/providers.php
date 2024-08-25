c291be587464a95499e981456b143a13ab09b331
Christopher Falkner <christopher.falkner@web.de>
2024-08-25 13:33:21 +0000

    fix(core): laravel/telescope installation

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,6 +4,5 @@ return [
     App\Providers\AdminCouncilsProvider::class,
     App\Providers\AppServiceProvider::class,
     App\Providers\HorizonServiceProvider::class,
-    App\Providers\TelescopeServiceProvider::class,
     App\Providers\VoltServiceProvider::class,
 ];
