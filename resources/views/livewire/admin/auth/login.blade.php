f8f1d6cbe1da9a43b6e86e81c6aa48084ac25e3d
Christopher Falkner <christopher.falkner@web.de>
2024-08-27 20:11:11 +0000

    style(admin): login form styled

--- resources/views/livewire/admin/auth/login.blade.php
+++ resources/views/livewire/admin/auth/login.blade.php
@@ -1,22 +1,26 @@
-<div>
-    <h2>
+<div class="bg-slate-100 rounded-lg px-5 w-fit mx-auto mt-10">
+    <h2 class="text-2xl pt-4 text-center">
         Login
     </h2>
-    <form wire:submit.prevent="login">
-        <div>
-            <label for="email">Email</label>
-            <input type="email" id="email" wire:model="email">
-            @error('email')
-                <span>{{ $message }}</span>
-            @enderror
+
+    {{-- session flashes --}}
+    @if (session()->has('error'))
+        <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mt-5 flex flex-col"
+            role="alert">
+            <strong class="font-bold">Fehler!</strong>
+            <span class="block sm:inline">{{ session('error') }}</span>
         </div>
-        <div>
-            <label for="password">Password</label>
-            <input type="password" id="password" wire:model="password">
-            @error('password')
-                <span>{{ $message }}</span>
-            @enderror
-        </div>
-        <button type="submit">Login</button>
-    </form>
+    @endif
+
+    <x-form wire:submit.prevent="login" class="flex gap-y-2 p-5">
+        <x-input type="email" label="E-Mail" id="email" wire:model="email" />
+        @error('email')
+            <span>{{ $message }}</span>
+        @enderror
+        <x-input type="password" label="Passwort" id="password" wire:model="password" />
+        @error('password')
+            <span>{{ $message }}</span>
+        @enderror
+        <x-button type="submit" label="Login" class="btn-primary btn-sm mt-3" />
+    </x-form>
 </div>
