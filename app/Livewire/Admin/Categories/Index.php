9607f3e286dfd6a95f1b124614491e5a43069bc0
Christopher Falkner <christopher.falkner@web.de>
2024-08-28 18:24:15 +0000

    style(admin): category listing styled

--- app/Livewire/Admin/Categories/Index.php
+++ app/Livewire/Admin/Categories/Index.php
@@ -18,6 +18,16 @@ class Index extends Component
     #[Url(except: '')]
     public $search = '';
 
+    public $headers = [
+        ['key' => 'council.name', 'label' => 'Gremium'],
+        ['key' => 'tag', 'label' => 'Tag'],
+        ['key' => 'name', 'label' => 'Name'],
+        ['key' => 'resolutions_count', 'label' => 'Beschlüsse'],
+        ['key' => 'actions', 'label' => 'Aktionen'],
+    ];
+
+    public $sortBy = ['column' => 'name', 'direction' => 'asc'];
+
     public function deleteCategory($id)
     {
         Category::findOrFail($id)->delete();
@@ -29,8 +39,16 @@ class Index extends Component
     public function render()
     {
         $categories = $this->search == ''
-            ? Category::where("council_id", session("councilId"))->paginate($this->perPage)
-            : Category::search($this->search)->where("council_id", session("councilId"))->paginate($this->perPage);
+            ? Category::where("council_id", session("councilId"))
+            ->withCount("resolutions")
+            ->with(['council:id,name'])
+            ->orderBy(...array_values($this->sortBy))
+            ->paginate($this->perPage)
+            : Category::search($this->search)->where("council_id", session("councilId"))
+            ->withCount("resolutions")
+            ->with(['council:id,name'])
+            ->orderBy(...array_values($this->sortBy))
+            ->paginate($this->perPage);
         return view('livewire.admin.categories.index', [
             'categories' => $categories
         ])->layout('layouts.admin');
