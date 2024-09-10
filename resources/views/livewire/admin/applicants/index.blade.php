df5f7c6156c8067c400ab45d25430b8a247f6062
Christopher <christopher.falkner@web.de>
2024-09-10 11:50:01 +0000

    feat(admin): add refresh listing btn

--- resources/views/livewire/admin/applicants/index.blade.php
+++ resources/views/livewire/admin/applicants/index.blade.php
@@ -1,5 +1,8 @@
 <div class="p-4">
-    <h2 class="text-2xl font-bold">Antragsteller*innen</h2>
+    <div class="flex flex-row items-center gap-x-4">
+        <h2 class="text-2xl font-bold">Antragsteller*innen</h2>
+        <x-button icon="o-arrow-path" wire:click="$refresh" class="btn-ghost btn-xs w-7 h-7" />
+    </div>
 
     @if (session()->has('success'))
         <div class="alert alert-success">
@@ -7,10 +10,14 @@
         </div>
     @endif
 
-    <div class="pt-7">
+    <div class="flex items-center justify-center min-h-screen min-w-screen" wire:loading.flex>
+        <x-loading class="text-primary loading-lg self-center mt-10" />
+    </div>
+
+    <div class="pt-7" wire:loading.class="hidden">
         <div id="listing-heading" class="pb-5 flex flex-row justify-between">
-            <x-input type="text" icon="o-magnifying-glass" class="input-sm" wire:model.live="search" placeholder="Suche"
-                id="search" />
+            <x-input type="text" icon="o-magnifying-glass" class="input-sm" wire:model.live="search"
+                placeholder="Suche" id="search" />
             <x-button wire:click="" label="Neue*n Antragsteller*in anlegen" class="btn-primary btn-sm text-white" />
         </div>
         <x-table :headers="$headers" :rows="$applicants" :sort-by="$sortBy" with-pagination per-page="perPage">
--- resources/views/livewire/admin/resolutions/index.blade.php
+++ resources/views/livewire/admin/resolutions/index.blade.php
@@ -1,5 +1,8 @@
-<div class="p-4">
-    <h2 class="text-2xl font-bold">Beschlüsse</h2>
+<div class="p-4 flex flex-col justify-center">
+    <div class="flex flex-row items-center gap-x-4">
+        <h2 class="text-2xl font-bold">Beschlüsse</h2>
+        <x-button icon="o-arrow-path" wire:click="$refresh" class="btn-ghost btn-xs w-7 h-7" />
+    </div>
 
     @if (session()->has('success'))
         <div class="alert alert-success">
@@ -7,17 +10,27 @@
         </div>
     @endif
 
-    <div class="pt-7">
+    <div class="flex items-center justify-center min-h-screen min-w-screen" wire:loading.flex>
+        <x-loading class="text-primary loading-lg self-center mt-10" />
+    </div>
+
+    <div class="pt-7" wire:loading.class="hidden">
         <div id="listing-heading" class="pb-5 flex flex-row justify-between">
-            <x-input type="text" icon="o-magnifying-glass" class="input-sm" wire:model.live="search" placeholder="Suche"
-                id="search" />
+            <x-input type="text" icon="o-magnifying-glass" class="input-sm" wire:model.live="search"
+                placeholder="Suche" id="search" />
             <x-button class="btn-primary btn-sm text-white " icon="o-plus"
                 href="{{ route('admin.resolutions.create') }}" wire:navigate>Neuen Beschluss anlegen</x-button>
         </div>
+
         <x-table :headers="$headers" :rows="$resolutions" :sort-by="$sortBy" with-pagination per-page="perPage">
             @scope('cell_applicants', $resolution)
                 @foreach ($resolution->applicants as $applicant)
-                    <a href="{{ route('admin.applicants.show', $applicant) }}" wire:navigate>{{ $applicant->name }}</a>
+                    <a href="{{ route('admin.applicants.index', $applicant) }}" wire:navigate>
+                        {{ $applicant->name }}
+                        @if (!$loop->last)
+                            &comma;
+                        @endif
+                    </a>
                 @endforeach
                 @if ($resolution->applicants->count() === 0)
                     <span>Keine Antragsteller*innen</span>
@@ -39,7 +52,6 @@
                         wire:navigate />
                 </div>
             @endscope
-
         </x-table>
     </div>
 </div>
